96d7ace9ad714bc6aadf44196c4ce6ef19295099
Liam Curry <liam@curry.name>
2013-01-08 18:13:44 -0500

    put some of the powerline stuff into it's own repo

--- .config/powerline/ext/tmux/segments/ip.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf8 -*-
-import urllib2
-
-
-url = 'http://automation.whatismyip.com/n09230945.asp'
-
-
-def external_ip():
-    return u'ⓦ  ' + unicode(urllib2.urlopen(url).read().strip())
